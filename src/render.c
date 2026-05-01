345ec549b7fffa069f77bfb33bf7e3e06a7fc01b
daedrafruit <160567370+daedrafruit@users.noreply.github.com>
2026-05-01 00:32:09 -0700

    header cleanup

--- src/render.c
+++ src/render.c
@@ -1,10 +1,8 @@
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_events.h>
-#include <SDL2/SDL_video.h>
-#include <SDL3/SDL_events.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include <wchar.h>
+
+#include <SDL2/SDL.h>
+
 #include <glad/glad.h>
 
 #define SCREEN_HEIGHT 480
